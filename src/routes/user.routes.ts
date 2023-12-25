4dee924be05cd1938db90db32bee6560b0249f24
Amit Kandar <kandaramit2001@gmail.com>
2023-12-26 02:15:09 +0530

    fixed the req.user error

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { signup } from "../controllers/user.controller";
+import { signin, signout, signup } from "../controllers/user.controller";
 import { upload } from "../middlewares/multer.middleware";
+import { checkAuth } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.route("/signup").post(upload.single("avatar"), signup)
+router.route("/signup").post(upload.single("avatar"), signup);
+router.route("/signin").post(signin);
 
+// Secured Routes
+router.route("/logout").get(checkAuth, signout);
 
 export default router;
