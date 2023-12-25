3d5ddbcd5f05bfb96b969baf7b2089d186e7a843
Amit Kandar <kandaramit2001@gmail.com>
2023-12-25 13:27:48 +0530

    fixed the database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants";
 
 export async function connectToDB(): Promise<void> {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log("MongoDB Connected:", connectionInstance.connection.host + "/" + connectionInstance.connection.name);
     } catch (error) {
         console.log("MongoDB connection Error: ", error);
