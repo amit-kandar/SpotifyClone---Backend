5182674d757c486ef8d8e08346bbf842a84e38a3
Amit Kandar <kandaramit2001@gmail.com>
2023-12-25 13:09:37 +0530

    Added cloudinary config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 import { connectToDB } from "./database";
 import { app } from "./app";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config({
     path: "./.env"
 })
 
 const PORT = process.env.PORT || 8000;
+
+// Define the configuration parameters
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || '',
+    api_key: process.env.CLOUDINARY_API_KEY || '',
+    api_secret: process.env.CLOUDINARY_API_SECRET || ''
+});
 
 connectToDB()
     .then(() => {
