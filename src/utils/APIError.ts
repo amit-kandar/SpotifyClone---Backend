8afc8d28430eb0c6e116c0c1fdb209ccdae42eef
Amit Kandar <kandaramit2001@gmail.com>
2023-12-25 22:36:00 +0530

    Add a file deletion function to APIError

--- src/utils/APIError.ts
+++ src/utils/APIError.ts
@@ -1,10 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
 class APIError extends Error {
-    statusCode: number
-    message: string
-    errors: Array<string>
-    stack?: string
-    data: any
-    success: boolean
+    statusCode: number;
+    message: string;
+    errors: Array<string>;
+    stack?: string;
+    data: any;
+    success: boolean;
 
     constructor(
         statusCode: number,
@@ -12,19 +15,46 @@ class APIError extends Error {
         errors: Array<string> = [],
         stack: string = ""
     ) {
-        super(message)
-        this.message = message
-        this.errors = errors
-        this.statusCode = statusCode
-        this.data = null
-        this.success = false
+        super(message);
+        this.message = message;
+        this.errors = errors;
+        this.statusCode = statusCode;
+        this.data = null;
+        this.success = false;
 
         if (stack) {
-            this.stack = stack
+            this.stack = stack;
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
+
+        // File deletion logic within the constructor
+        /**
+         * when we get error and also multer middleware add image in temp folder.
+         * That time we should remove the file from temp.
+         */
+        const directoryPath = 'public/temp/';
+
+        fs.readdir(directoryPath, (err: NodeJS.ErrnoException | null, files: string[]) => {
+            if (err) {
+                console.error('Error reading directory:', err);
+                return;
+            }
+
+            files.forEach((file) => {
+                if (path.extname(file) === '.png') {
+                    const filePath = path.join(directoryPath, file);
+                    fs.unlink(filePath, (error: NodeJS.ErrnoException | null) => {
+                        if (error) {
+                            console.error(`Error deleting file ${filePath}:`, error);
+                        }
+                    });
+                }
+            });
+        });
     }
+
+
 }
 
-export { APIError }
+export { APIError };
