9bcc4324740bbc8b8eb48300d2b6efcdc11e3df3
Amit Kandar <kandaramit2001@gmail.com>
2023-12-26 10:47:00 +0530

    fixed the req.user bugs

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,9 +3,10 @@ import { Request } from 'express';
 import { UserDocument } from '../models/user.model';
 
 // Augment the 'express' Request type definition to include the 'user' property
-declare module 'express' {
-    interface Request {
-        user?: UserDocument; // Replace 'any' with the actual type of your user object
-        // Add any other custom properties or methods related to user information
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserDocument; // Define 'user' property
+        }
     }
 }
