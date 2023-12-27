35ac8117b4df0905ff1bbc5a2951800d831780f1
Amit Kandar <kandaramit2001@gmail.com>
2023-12-27 22:44:53 +0530

    Add timestamp to APIResponse

--- src/utils/APIResponse.ts
+++ src/utils/APIResponse.ts
@@ -3,6 +3,7 @@ class APIResponse {
     data: any
     message: string
     success: boolean
+    timestamp: Date;
     constructor(
         statusCode: number,
         data: any,
@@ -12,6 +13,7 @@ class APIResponse {
         this.data = data
         this.message = message
         this.success = statusCode < 400
+        this.timestamp = new Date()
     }
 }
 
