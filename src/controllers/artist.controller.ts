3f1ecc2d52a5a4ec5f35dab3e549e70e832ba02b
Amit Kandar <kandaramit2001@gmail.com>
2023-12-31 12:47:00 +0530

    Added new routes and controllers for artists.

--- src/controllers/artist.controller.ts
+++ src/controllers/artist.controller.ts
@@ -5,6 +5,7 @@ import { Artist } from "../models/artist.model";
 import { APIResponse } from "../utils/APIResponse";
 import { User } from "../models/user.model";
 import mongoose, { Schema } from "mongoose";
+import { Follower } from "../models/follower.model";
 
 const genres = [
     "rock",
@@ -112,6 +113,272 @@ export const updateArtistProfile = asyncHandler(async (req: Request, res: Respon
 
 
     } catch (error) {
+        next(error);
+    }
+});
+
+// @route   GET /api/v1/artsists/:id
+// @desc    Get artist by id
+// @access  Private
+// have to check when userId and artist model user are not same
+export const getArtistById = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get user id from req.user.id
+    const userId = req.user?.id;
+
+    // get id from params
+
+    const artistId = new mongoose.Types.ObjectId(req.params.id);
+
+    try {
+        // check for valid request
+        if (!userId) throw new APIError(401, "Invalid request, signin again");
+
+        // validate artist
+        if (!artistId) throw new APIError(404, "No such artist found");
+        // find artist by id
+        const artist = await Artist.aggregate([
+            { $match: { _id: artistId } },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "creator"
+                }
+            },
+            {
+                $lookup: {
+                    from: "songs",
+                    localField: "_id",
+                    foreignField: "artist",
+                    as: "songs"
+                }
+            },
+            {
+                $unwind: {
+                    path: "$creator"
+                }
+            },
+            {
+                $unwind: {
+                    path: "$songs",
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $project: {
+                    "creator.password": 0,
+                    "creator.refreshToken": 0,
+                }
+            }
+        ]);
+
+        // verify artist
+        if (!artist || !artist[0]) throw new APIError(400, "artist Not found");
+
+        // send response
+        res
+            .status(200)
+            .json(new APIResponse(
+                200,
+                { Artist: artist[0] },
+                "User details fetched successfully"
+            ))
+    } catch (error) {
+        next(error);
+    }
+});
+
+// @route   GET /api/v1/artists/
+// @desc    Add song
+// @access  Private
+export const getALlArtists = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get user id from req.user.id
+    const userId = req.user?.id;
+    try {
+        // check for valid request
+        if (!userId) throw new APIError(401, "Invalid request, signin again");
+
+
+        // retrive artist with user details
+        const artists = await Artist.aggregate([
+            {
+                $lookup: {
+                    from: "users", // Name of the user collection
+                    localField: "user", // Field in the artist collection
+                    foreignField: "_id", // Field in the user collection
+                    as: "details" // Output array field containing user details
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    __v: 0,
+                    "details._id": 0,
+                    "details.createdAt": 0,
+                    "details.updatedAt": 0,
+                    "details.__v": 0,
+                    "details.password": 0,
+                    "details.refreshToken": 0
+                }
+            }
+        ]);
+
+        // send response to user
+        res
+            .status(200)
+            .json(new APIResponse(
+                200,
+                { Artists: artists },
+                "Artists fetched successfully"
+            ));
+    } catch (error) {
+        next(error);
+    }
+});
+
+// @route   POST /api/v1/artists/:id/follow
+// @desc    Add song
+// @access  Private
+// may be need some changing on where artist id should provide(like, path variable, query, or body)
+export const followArtist = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get userid from req.user.id
+    const userId = req.user?.id;
+
+    // get artist id from params
+    const artistId = req.params.id;
+
+    try {
+        // check for valid request
+        if (!userId) throw new APIError(401, "Invalid request, signin again");
+
+        // validate artist
+        if (!artistId) throw new APIError(404, "No such artist found");
+
+        // create follow documment
+        const response = await Follower.create({
+            user: userId,
+            artist: artistId
+        });
+
+        // validate response
+        if (!response) throw new APIError(400, "Error while creating follow request");
+
+        res
+            .status(201)
+            .json(new APIResponse(
+                201,
+                { Follower: response },
+                "Follow request sent successfully"
+            ));
+    } catch (error) {
+        next(error);
+    }
+});
+
+// @route GET /api/v1/artists/following-artists
+// @desc Get all the artist whom a perticuler user following
+// @access private
+export const followingArtistByUser = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+    // get userId from req.user.id
+    const userId = new mongoose.Types.ObjectId(req.user?.id);
+
+    try {
+        // check for valid request
+        if (!userId) throw new APIError(401, "Invalid request, signin again");
+
+        // retrive all artist that the perticular user follow
+        const allFollowingArtists = await Follower.aggregate([
+            {
+                $match: { user: userId }
+            },
+            {
+                $lookup: {
+                    from: "artists",
+                    let: { artistId: "$artist" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ["$_id", "$$artistId"] }
+                            }
+                        },
+                        {
+                            $project: {
+                                _id: 0,
+                                createdAt: 0,
+                                updatedAt: 0,
+                                __v: 0
+                            }
+                        }
+                    ],
+                    as: "Artist"
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    let: { userId: "$user" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ["$_id", "$$userId"] }
+                            }
+                        },
+                        {
+                            $project: {
+                                _id: 0,
+                                createdAt: 0,
+                                updatedAt: 0,
+                                __v: 0,
+                                password: 0,
+                                refreshToken: 0
+                            }
+                        }
+                    ],
+                    as: "Details"
+                }
+            },
+            {
+                $unwind: {
+                    path: "$Artist",
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $unwind: "$Details"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    user: 0,
+                    artist: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    __v: 0
+                }
+            }
+        ]);
+
+        // console.log(allFollowingArtists);
+
+
+        // validate response
+        if (!allFollowingArtists) throw new APIError(400, "Error while fetching following artists!");
+
+        // send response to user
+        res
+            .status(200)
+            .json(new APIResponse(
+                200,
+                { Following: allFollowingArtists },
+                "Successfully fetched all artists whom you follow"
+            ))
+    } catch (error) {
+        console.log(error);
+
         next(error);
     }
 });
--- src/routes/artist.routes.ts
+++ src/routes/artist.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { createArtistProfile, updateArtistProfile } from "../controllers/artist.controller";
+import { createArtistProfile, getArtistById, getALlArtists, updateArtistProfile, followArtist, followingArtistByUser } from "../controllers/artist.controller";
 import { checkRole } from "../middlewares/permission.middleware";
 import { checkAuth } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/create").post(checkAuth, checkRole(["admin", "regular"]), createArtistProfile);
-
+router.route("/following-artists").get(checkAuth, followingArtistByUser);
+router.route("/").get(checkAuth, getALlArtists);
 router.route("/:id").put(checkAuth, checkRole(["admin", "artist"]), updateArtistProfile);
-
-
+router.route("/:id").get(checkAuth, getArtistById);
+router.route("/:id/follow").post(checkAuth, followArtist);
 
 export default router;
