da848496ff2fbc3333d2ffd7e8d9d490a021d822
Amit Kandar <kandaramit2001@gmail.com>
2024-01-06 21:06:37 +0530

    Added artist details inside the req.user

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import logger from "../config/logger";
 import redisClient from "../config/redis";
+import { Artist } from "../models/artist.model";
 import { User } from "../models/user.model";
 import { APIError } from "../utils/APIError";
 import { asyncHandler } from "../utils/asyncHandler";
@@ -12,54 +13,51 @@ export const checkAuth = asyncHandler(async (req: Request, res: Response, next: 
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
         // if token not found send unauthorized user
-        if (!token) {
-            const errorMessage = "Unauthorized Request, signin again";
-            logger.warn(`Authentication failed: ${errorMessage}`);
-            throw new APIError(401, errorMessage);
-        }
+        if (!token)
+            throw new APIError(401, "Unauthorized Request, signin again");
 
         // fetch token secret
         const secret: string | undefined = process.env.ACCESS_TOKEN_SECRET;
-        if (!secret) {
-            const errorMessage = "Secret not found in environment variables";
-            logger.error(errorMessage);
-            throw new APIError(500, errorMessage);
-        }
+        if (!secret)
+            throw new APIError(500, "Secret not found in environment variables");
 
         // validate access token and store decoded token
         const decoded: JwtPayload | string = jwt.verify(token, secret);
-        if (typeof decoded === "string") {
-            const errorMessage = "Invalid decoded information";
-            logger.error(errorMessage);
-            throw new APIError(400, errorMessage);
-        }
+        if (typeof decoded === "string")
+            throw new APIError(400, "Invalid decoded information");
 
-        // retrieve the user
+        // retrieve the user from redis
         let userData;
         try {
-            userData = await redisClient.get(`user:${decoded._id}`);
+            userData = await redisClient.get(`${decoded._id}`);
         } catch (err) {
-            const errorMessage = "Error while fetching details from redis";
-            logger.error(errorMessage, { error: err });
-            throw new APIError(500, errorMessage);
+            throw new APIError(500, "Error while fetching details from redis");
         }
-
         let user;
         if (!userData) {
             user = await User.findById(decoded._id).select("-password -refreshToken");
-            if (!user) {
-                const errorMessage = "Invalid access token";
-                logger.error(errorMessage);
-                throw new APIError(404, errorMessage);
-            }
+            if (!user)
+                throw new APIError(404, "Invalid access token");
         } else {
             user = JSON.parse(userData);
         }
 
-        // set user into req.user
-        req.user = user;
+        if (user?.role === "artist") {
+            const artist = await Artist.findOne({ user: user._id });
 
-        logger.info(`Authentication successful for user: ${user.username}`);
+            if (!artist)
+                throw new APIError(404, "Invalid access token");
+
+            req.user = {
+                ...user,
+                details: { ...artist.toObject() }
+            };
+        } else {
+            req.user = user;
+        }
+
+        //log on successfull authentication
+        logger.info(`Authentication successful for user: ${user?.username}`);
 
         // call next()
         next();
